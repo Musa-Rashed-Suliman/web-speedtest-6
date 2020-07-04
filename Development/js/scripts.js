01189ad4b501782d9968353c6271646a09948bb9
Musa Alabdulhadi <houdq8@gmail.com>
2020-07-04 17:32:18 +0300

    done and i cant javascript ): im very sorry

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -7,11 +7,32 @@
  * - name إن استقبلت اسم أو "Hello <name>" تطبع
  */
 
-function greet(name) {}
-
+function greet(name) {
+    let message = "Hello ";
+    if(name) {
+        message += name;
+        console.log(message);
+    }else{
+        console.log(message);
+    }
+    
+}
+greet();
+greet("Musa");
 /*
  * isOdd(n) اسم الدالة
  * - n تستقبل رقم
  * - عدا ذلك false إن كان الرقم فردياً وترجع true ترجع القيمة
  */
-function isOdd(n) {}
+function isOdd(n) {
+if (n % 2 == 1){
+    return true;
+}
+else{
+
+return false;
+
+}
+}
+console.log(isOdd(5));
+console.log(isOdd(2));
